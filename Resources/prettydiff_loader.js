aa76ce4cdd1f204dcc292fdc1760d94ae75132bc
Matthew Casperson <matthewcasperson@gmail.com>
2013-02-12 09:01:55 +1000

    Working on new report

--- Resources/prettydiff_loader.js
+++ Resources/prettydiff_loader.js
@@ -13,7 +13,7 @@ function displayDiff(source, sourceLabel, diff, diffLabel, isXML)
     var win = window.open("", "_blank", "width=" + (screen.width - 200) + ", height=" + (screen.height - 200) + ",scrollbars=yes"); // a window object
     if (win != null) {
         win.document.open("text/html", "replace");
-        win.document.write("<HTML><HEAD><TITLE>PressGangCCMS XML Diff</TITLE><link rel=\"stylesheet\" type=\"text/css\" href=\"../prettydiff.css\"><link rel=\"stylesheet\" type=\"text/css\" href=\"prettydiff.css\"></HEAD><BODY>"
+        win.document.write("<HTML><HEAD><TITLE>PressGangCCMS XML Diff</TITLE><link rel=\"stylesheet\" type=\"text/css\" href=\"/birt/Resources/prettydiff.css\"><link rel=\"stylesheet\" type=\"text/css\" href=\"prettydiff.css\"></HEAD><BODY>"
                 + diffTable + "</BODY></HTML>");
         win.document.close();
     }
