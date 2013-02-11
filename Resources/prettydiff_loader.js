c7f4cef5d04812cdf73e973a82a2a39d32e2af74
Matthew Casperson <matthewcasperson@gmail.com>
2013-02-12 08:01:21 +1000

    Working on new report

---
+++ Resources/prettydiff_loader.js
@@ -0,0 +1,20 @@
+function displayDiff(final String source, final String sourceLabel, final String diff, final String diffLabel, final boolean isXML)
+{
+    var diffTable = prettydiff({
+        source : source,
+        sourcelabel : sourceLabel,
+        diff : diff,
+        difflabel : diffLabel,
+        lang : isXML ? "markup" : "text",
+        mode : "diff",
+        diffview : "sidebyside"
+    })[0];
+
+    var win = window.open("", "_blank", "width=" + (screen.width - 200) + ", height=" + (screen.height - 200) + ",scrollbars=yes"); // a window object
+    if (win != null) {
+        win.document.open("text/html", "replace");
+        win.document.write("<HTML><HEAD><TITLE>PressGangCCMS XML Diff</TITLE><link rel=\"stylesheet\" type=\"text/css\" href=\"../prettydiff.css\"><link rel=\"stylesheet\" type=\"text/css\" href=\"prettydiff.css\"></HEAD><BODY>"
+                + diffTable + "</BODY></HTML>");
+        win.document.close();
+    }
+}
